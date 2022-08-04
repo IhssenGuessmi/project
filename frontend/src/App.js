f59e2498aaa785517c72f68051913a5ba30d6e57
Ihssen Guessmi <ihssenguessmi@gmail.com>
2022-08-04 18:58:01 +0100

    component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
+import NavScrollExample from "./components/Navbar";
 
 
 function App() {
   return (
-    <div className="App">
-      
+    <div>
+    <h1>project 5lat fel min 20 arja3 9bal b 1 min w tfarej w testarti b npm run dev fel project</h1>
+    <NavScrollExample/>
     </div>
   );
 }
